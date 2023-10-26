50ed2d611ea9a7e38f26e8dca1062c48a2bdeb9f
César Jorge Maalouf Junior <cesarjorgemjr@gmail.com>
2023-10-26 12:22:24 -0300

    First Commit

---
+++ Assets/Char_Cesar/Char_Cesar.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char_Cesar : CharBase
+{
+    private Dictionary<int, string> periodToLocation = new Dictionary<int, string>();
+    private Vector3 targetPosition;
+    [SerializeField] private float moveSpeed = 1f;
+    private void Update()
+    {
+        //Nao precisa mexer
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed / 10 * Time.deltaTime);
+    }
+    private void AdicionarARotina(int periodoDoDia, string lugar)
+    {
+        periodToLocation.Add(periodoDoDia, lugar);
+    }
+
+    //Para implementacao simples mexer apenas a baixo
+
+    /*
+    Nome dos lugares no mapa:
+    TownSquare
+    Bakery
+    Bar
+    Library
+    Hospital
+    ?
+    */
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Nao mexer nessa funcao ate a implementacao de interacoes especificas com alguns personagens
+        if (collision.gameObject.tag == "Char")
+        {
+            if (collision.TryGetComponent<CharBase>(out CharBase charBase))
+            {
+                charBase.Interact(this);
+            }
+            else
+            {
+                Debug.Log("Erro em pegar informacoes de" + collision.gameObject.name);
+            }
+        }
+    }
+    void Start()
+    {
+        AdicionarARotina(0, "Bakery");
+        AdicionarARotina(1, "Bar");
+        AdicionarARotina(2, "TownSquare");
+        AdicionarARotina(3, "Library");
+        AdicionarARotina(4, "Bar");
+        
+        //Nao mexer na linha a baixo
+        targetPosition = transform.position;
+        GameManager.onChangePeriod.AddListener(OnChangePeriod);
+    }
+    public override void Interact(CharBase charInfo)
+    {
+        if (charInfo.Persona == PersonalityT.Flirty)
+        {
+            humor += 1;
+        }
+    }
+
+    public void OnChangePeriod(int periodo)
+    {
+        if (periodToLocation.ContainsKey(periodo))
+        {
+            Vector3 locationObject = GameManager._placePosition[periodToLocation[periodo]];
+
+            if (locationObject != null)
+            {
+                targetPosition = locationObject;
+            }
+        }
+    }
+}
