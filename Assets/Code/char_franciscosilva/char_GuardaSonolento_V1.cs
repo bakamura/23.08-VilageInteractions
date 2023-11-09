bb84cf8e899ba44e39c8eaada5806ed59394c699
Chi-ih-co <88665676+Chi-ih-co@users.noreply.github.com>
2023-11-09 12:45:03 -0300

    update-char_GuardaSonolento

--- Assets/Code/char_franciscosilva/char_GuardaSonolento_V1.cs
+++ Assets/Code/char_franciscosilva/char_GuardaSonolento_V1.cs
@@ -29,9 +29,20 @@ public class char_GuardaSonolento_V1 : CharBase
     ?
     */
 
+    private void FixedUpdate()
+    {
+        if (tired == 3)
+        {
+        targetPosition = collision.gameObject.name.TownSquare;
+        }
+    }
+
+    [SerializeField, Range(-3, 3)] protected float tired;
+    public float Tired { get { return tired; } }
+    //relacionado ao sono
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        //Nao mexer nessa funcao ate a implementacao de interacoes especificas com alguns personagens
         if (collision.gameObject.tag == "Char")
         {
             if (collision.TryGetComponent<CharBase>(out CharBase charBase))
@@ -43,6 +54,32 @@ public class char_GuardaSonolento_V1 : CharBase
                 Debug.Log("Erro em pegar informacoes de" + collision.gameObject.name);
             }
         }
+        else
+        {
+            //Reacao do npc baseado na posicao de mundo
+            switch (collision.gameObject.name)
+            {
+                case "TownSquare":
+                    tired += 1;
+                    break;
+                case "Bakery":
+                    break;
+                case "Bar":
+                    tired -= 1;
+                    humor -= 1;
+                    break;
+                case "Library":
+                    tired += 1;
+                    humor += 1;
+                    break;
+                case "Hospital":
+                    tired += 1;
+                    humor -= 1;
+                    break;
+                case "?":
+                    break;
+            }
+        }
     }
     void Start()
     {
@@ -51,7 +88,9 @@ public class char_GuardaSonolento_V1 : CharBase
         AdicionarARotina(2, "Library");
         AdicionarARotina(3, "TownSquare");
         AdicionarARotina(4, "Bar");
-        AdicionarARotina(5, "TownSquare");
+        AdicionarARotina(5, "Hospital");
+        AdicionarARotina(6, "TownSquare");
+
 
         //Nao mexer na linha a baixo
         targetPosition = transform.position;
@@ -59,23 +98,48 @@ public class char_GuardaSonolento_V1 : CharBase
     }
     public override void Interact(CharBase charInfo)
     {
+        if (charInfo.gender == GenderT.Female)
+        {
+            persona = PersonalityT.Shy;
+        } 
         switch(charInfo.Persona)
         {
             case PersonalityT.Shy:
             humor += 1;
+            tired += 1;
             break;
 
             case PersonalityT.Kind:
             humor += 1;
+            tired -= 1;
             break;
 
-            case PersonalityT.Sado:
+            case PersonalityT.Sadistic:
             humor -= 1;
+            tired += 1;
             break;
 
             case PersonalityT.Loud:
             humor -= 2;
+            tired -= 1;
+                if (humor < -1) 
+                {
+                    persona = PersonalityT.Loud;
+                }
+                if (charInfo.targetPosition == GameObject.Find("Library").transform.position)
+                {
+                    targetPosition = GameObject.Find("?").transform.position;
+                }
+                else
+                {
+                    targetPosition = GameObject.Find("Library").transform.position;
+                }
+            break;
+            //se encontrar com algm Loud, o guarda tentará fugir em direção à biblioteca. se o humor estiver abaixo de -1, ele se torna Loud.
         }
+        //interações relacionadas ao fator de Persona
+
+        
     }
 
     public void OnChangePeriod(int periodo)
